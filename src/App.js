813695de16b8e5b2936c49bbd96a18498a705625
muhammadganny <mohammadganny@gmail.com>
2023-08-04 17:36:36 +0500

    linking

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import Reeact from 'react'
-//import './App.css';
-import './index';
-import Navbar from "./components/Navbar"
-import Button from '@mui/material/Button';
-import ADD from "./components/Add"
-import About from "./components/About"
+import React from 'react';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'; // Import Route
+import Navbar from './components/Navbar';
 
+import About from './components/About'; // Update the import
+import Add from './components/Add'; // Update the import
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <div className="container">
-        <h1 className="display">
-          <span className="text-prim">Contact </span>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <h1 className="display">
+            <span className="text-prim">Contact </span>
             List
-        </h1>
-      </div> 
-       {/* <ADD/>
-      <About/> */}
-    </div>
+          </h1>
+        </div>
+        <Routes>
+          <Route path="/about" component={About} />
+          <Route path="/contact/add" component={Add} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+// import Reeact from 'react'
+// import './index';
+// import Navbar from "./components/Navbar"
+// import Button from '@mui/material/Button';
+// import ADD from "./components/Add"
+// import About from "./components/About"
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Navbar/>
+//       <div className="container">
+//         <h1 className="display">
+//           <span className="text-prim">Contact </span>
+//             List
+//         </h1>
+//       </div> 
+//        <ADD/>
+//       <About/>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,65 +1,114 @@
-import React from "react"
+import React from 'react';
+import { Link } from 'react-router-dom';
 import HomeIcon from '@mui/icons-material/Home';
 import InfoIcon from '@mui/icons-material/Info';
 import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 
-
 function Navbar() {
-    return(
-        <nav className="navbar">
-            <div className="container">
-               <h2 className="nav-brand">Contact Maneger</h2> 
-               {/* not linking anything to it  */}
-                <div>
-                    <ul className="nav-option">
-                        <li className="nav-item">
-                            <a className="nav-link" href="/">
-                                <HomeIcon/>
-                                <span className="nav-text">Home</span>
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/contact/add">
-                                <AddOutlinedIcon/>
-                                <span className="nav-text">Add</span>
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/about">
-                                <InfoIcon/>
-                                <span className="nav-text">About</span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-    )
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <h2 className="nav-brand">Contact Manager</h2>
+        <div>
+          <ul className="nav-option">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                <HomeIcon />
+                <span className="nav-text">Home</span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact/add">
+                <AddOutlinedIcon />
+                <span className="nav-text">Add</span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                <InfoIcon />
+                <span className="nav-text">About</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
 
 
-{/* <div className="nav">
-        <div className="div-container">
-          <div className="link">
-            <div className="text-wrapper">Contact Manager</div>
-          </div>
-          <ul className="list">
-            <div className="item-link">
-              {/* <AccessTimeFilledIcon/> */}
-        //       <HomeIcon fontSize="small" background-color ="white" />
-        //       <li className="home"> Home</li>
-        //     </div>
-        //     <div className="div">
-        //        <AddOutlinedIcon/>
-        //       <li className="add"> Add</li>
-        //     </div>
-        //     <div className="item-link-2">
-        //       <InfoIcon />
-        //       <li className="about"> About</li>
-        //     </div>
-        //   </ul>
-        // </div>
-    //  </div> */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react"
+// import HomeIcon from '@mui/icons-material/Home';
+// import InfoIcon from '@mui/icons-material/Info';
+// import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+
+
+// function Navbar() {
+//     return(
+//         <nav className="navbar">
+//             <div className="container">
+//                <h2 className="nav-brand">Contact Maneger</h2> 
+//                {/* not linking anything to it  */}
+//                 <div>
+//                     <ul className="nav-option">
+//                         <li className="nav-item">
+//                             <a className="nav-link" href="/">
+//                                 <HomeIcon/>
+//                                 <span className="nav-text">Home</span>
+//                             </a>
+//                         </li>
+//                         <li className="nav-item">
+//                             <a className="nav-link" href="/contact/add">
+//                                 <AddOutlinedIcon/>
+//                                 <span className="nav-text">Add</span>
+//                             </a>
+//                         </li>
+//                         <li className="nav-item">
+//                             <a className="nav-link" href="/about">
+//                                 <InfoIcon/>
+//                                 <span className="nav-text">About</span>
+//                             </a>
+//                         </li>
+//                     </ul>
+//                 </div>
+//             </div>
+//         </nav>
+
+//     )
+// }
+
+// export default Navbar
+
+
