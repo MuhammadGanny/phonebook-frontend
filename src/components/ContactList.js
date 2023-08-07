2e60decbbdc8284a5246e4cc74b3bc1a34155c70
muhammadganny <mohammadganny@gmail.com>
2023-08-07 18:14:17 +0500

    update not done

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Navbar from "./Navbar";
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 function ContactList(){
 
@@ -32,7 +33,9 @@ function ContactList(){
         }
     }
       
-
+     const state = {
+        showContactInfo: false
+      };
 
     return(
         <>
@@ -47,15 +50,19 @@ function ContactList(){
                     <div className="col-md-4 mb-4" key={contact.id}>
                         <div className="card">
                          <div className="card-body">
-                            <h4 className="card-title">{contact.name}</h4>
-                            <ul className="list-group">
-                                <li className="bg-light list-group-item">
-                                    "Email:" {contact.email}
-                                </li>
-                                <li className="bg-light list-group-item">
-                                    "Phone: " {contact.phone}
-                                </li>
-                            </ul>
+                            <h4 className="card-title">{contact.name}
+                                <ArrowDropDownIcon/>
+                            </h4>
+                            
+                                <ul className="list-group">
+                                    <li className="bg-light list-group-item">
+                                        "Email:" {contact.email}
+                                    </li>
+                                    <li className="bg-light list-group-item">
+                                        "Phone: " {contact.phone}
+                                    </li>
+                                </ul>
+                           
                             {/* <p className="card-text">{contact.email}</p>
                             <p className="card-text">{contact.phone}</p> */}
                             <button
@@ -68,18 +75,7 @@ function ContactList(){
                         </div>
                     </div>
                     ))}
-                </div>
-            
-                {/* <h2>Contact List</h2> */}
-                {/* <ul>
-                    {contacts.map((contact) => (
-                    <li key={contact.id}>
-                        {contact.name} - {contact.email} - {contact.phone}
-                        <button onClick={() => deleteContact(contact.id)}>Delete</button>
-                    </li>
-                    ))}
-                </ul> */}
-         
+                </div>        
                
             </div>
         </>
