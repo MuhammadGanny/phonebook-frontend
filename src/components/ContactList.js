bbfdb157b30cabaa313e4227130260ab59ecc6c2
muhammadganny <mohammadganny@gmail.com>
2023-08-07 17:58:26 +0500

    delete option working ui changes

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -42,17 +42,46 @@ function ContactList(){
                     <span className="text-prim">Contact </span>
                     List
                 </h1>
+                <div className="row">
+                    {contacts.map((contact) => (
+                    <div className="col-md-4 mb-4" key={contact.id}>
+                        <div className="card">
+                         <div className="card-body">
+                            <h4 className="card-title">{contact.name}</h4>
+                            <ul className="list-group">
+                                <li className="bg-light list-group-item">
+                                    "Email:" {contact.email}
+                                </li>
+                                <li className="bg-light list-group-item">
+                                    "Phone: " {contact.phone}
+                                </li>
+                            </ul>
+                            {/* <p className="card-text">{contact.email}</p>
+                            <p className="card-text">{contact.phone}</p> */}
+                            <button
+                            className="btn btn-danger"
+                            onClick={() => deleteContact(contact.id)}
+                            >
+                            Delete
+                            </button>
+                          </div>
+                        </div>
+                    </div>
+                    ))}
+                </div>
+            
+                {/* <h2>Contact List</h2> */}
+                {/* <ul>
+                    {contacts.map((contact) => (
+                    <li key={contact.id}>
+                        {contact.name} - {contact.email} - {contact.phone}
+                        <button onClick={() => deleteContact(contact.id)}>Delete</button>
+                    </li>
+                    ))}
+                </ul> */}
+         
+               
             </div>
-            <h2>Contact List</h2>
-            <ul>
-                {contacts.map((contact) => (
-                <li key={contact.id}>
-                    {contact.name} - {contact.email} - {contact.phone}
-                    <button onClick={() => deleteContact(contact.id)}>Delete</button>
-                </li>
-                ))}
-            </ul>
-        
         </>
     )
 }
