d867068cc7cc3a851ee4f34a88190ce1bc188843
muhammadganny <mohammadganny@gmail.com>
2023-08-22 12:59:50 +0500

    final

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -20,7 +20,7 @@ function ContactList() {
   
     const fetchContacts = async () => {
       try {
-        const response = await axios.get(`http://localhost:4000/api/phonebook`);
+        const response = await axios.get(`https://phonebook-backend-lo11.onrender.com/api/phonebook`);
         setContacts(response.data);
       } catch (error) {
         console.error('Error fetching contacts:', error);
